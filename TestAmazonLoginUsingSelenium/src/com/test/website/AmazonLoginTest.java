cc85fdfd5d0f828ef970633bc75dccb466752e68
Devendra Talele <62703377+Devendra274@users.noreply.github.com>
2021-05-30 21:33:26 +0530

    Amazon Login Testing

---
+++ TestAmazonLoginUsingSelenium/src/com/test/website/AmazonLoginTest.java
@@ -0,0 +1,28 @@
+package com.test.website;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AmazonLoginTest {
+
+	public static void main(String[] args) {
+//		https://www.amazon.in/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.in%2F%3Fref_%3Dnav_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=inflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&
+		System.setProperty("webdriver.chrome.driver", "/home/dev2000/ACTS/Software Engg./Testing/Selenium/chromedriver_linux64/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.amazon.in/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.in%2F%3Fref_%3Dnav_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=inflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&");
+		
+		WebElement email = driver.findElement(By.xpath("//input[@type='email']"));
+		email.sendKeys("devendratalele2000@gmail.com");
+		
+		int count = 0;
+		if(count == 0) {
+			email.clear();
+		}
+		
+		WebElement clickContinue = driver.findElement(By.id("continue"));
+		clickContinue.click();
+	}
+
+}
